748e4410cfb8a365109649bb5c9d7635c0d136a5
Tom Page <thomas.page.ext@adevinta.com>
2025-11-25 15:55:45 +0100

    yet more

--- src/functionality/generateToken.tsx
+++ src/functionality/generateToken.tsx
@@ -28,8 +28,8 @@ async function base64encode(input: (plain: string) => Promise<ArrayBuffer>): Pro
 
 
 const clientId = '6051dc7872fc4d978f770adfc1bcdaf7';
-// const redirectUri = 'https://t-page.github.io/blog/';
-const redirectUri = 'https://192.168.1.128:3000/blog';
+const redirectUri = 'https://t-page.github.io/blog/';
+// const redirectUri = 'https://192.168.1.128:3000/blog';
 
 const scope: string = 'user-top-read';
 const authUrl: URL = new URL("https://accounts.spotify.com/authorize?")
